6d552a98ed3ed1fdfc52736c798e9e5d7a3c4ee7
deeredman1991 <deeredman1991@gmail.com>
2016-08-09 11:00:12 -0400

    fixing glitchy asteroid bug

--- Scripts/Asteroid.py
+++ Scripts/Asteroid.py
@@ -9,7 +9,7 @@ class Asteroid(Sprite):
     def __init__(self, scale, background=None, **kwargs):
     
         self.metal = True if random.randint(1,2) == 1 else False
-    
+        
         self.source = 'images/{}Asteroid{}.png'.format('Metal' if self.metal else '', random.randint(1,5))
         
         super(Asteroid, self).__init__(scale*random.uniform(0.75, 1), **kwargs)
@@ -27,11 +27,11 @@ class Asteroid(Sprite):
         self.on_start()
         
     def on_start(self):
-        self.pos = (random.randint(0, int(self.background_width-self.width)), int(self.background_height))
         self.rot_velocity = random.randrange(-4, 4)
         self.speed = random.uniform(3, 5)
         self.velocity_x = random.uniform(-self.speed*0.1, self.speed*0.1)
         self.velocity_y = self.speed
+        
         self.collision = False
         
         self.score_value = 10
@@ -50,8 +50,16 @@ class Asteroid(Sprite):
             
         if self.collision == True:
             self.parent.enemyList.remove(self)
-            self.parent.standby_enemyList.append(self)
-            #self.parent.remove_widget(self)
+            
+            #Optimized to recycle asteroids but causes graphical errors
+            #self.parent.standby_enemyList.append(self)
+            
+            #temporary
+            parent = self.parent
+            self.parent.remove_widget(self)
+            self.pos = (random.randint(0, int(self.background_width-self.width)), self.background_height+self.height+self.width)
+            parent.standby_enemyList.append(self)
+            parent.add_widget(self)
             
     def on_killed(self):
         self.y = -self.height
